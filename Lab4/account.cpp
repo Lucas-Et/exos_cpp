14f3a5afbfe56d4ed01bbe7266fecbc88726c09b
SpaceLauncher <lucas.etourneau1@outlook.fr>
2018-11-12 15:51:59 +0100

    Yo

---
+++ Lab4/account.cpp
@@ -0,0 +1,49 @@
+#include "account.h"
+
+using namespace std;
+
+Account::Account(int number, double balance): m_number(number), m_balance(balance){}
+
+void Account::credit(double a) {
+  m_balance += a;
+}
+
+double Account::debit(double a) {
+  m_balance -= a;
+  return a;
+}
+
+unsigned int Account::acc() {
+  return m_number;
+}
+
+void Account::print(std::ostream &o) {
+  o << "Account n° : " << m_number << ", balance : " << m_balance << "€"<< endl;
+}
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
+
+Current_Account::Current_Account(int number, double balance):Account(number, balance){}
+
+double Current_Account::debit(double a) {
+  if (m_balance >= a) {
+    m_balance -= a;
+    return a;
+  }
+  else if (m_balance = 0) {
+    return 0;
+  }
+  else {
+    double temp = m_balance;
+    m_balance = 0;
+    return temp;
+  }
+}
+
+Saving_Account::Saving_Account(int number, double balance, double interest_rate):Account(number, balance), m_interest_rate(interest_rate){}
+
+void Saving_Account::add_intersts() {
+  m_balance += m_balance*m_interest_rate;
+}
---
+++ Lab4/account.h
@@ -0,0 +1,53 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+
+#define IRATEBLOCKED 0.04
+#define IRATEUNBLOCKED 0.02
+
+class Account{
+protected:
+  unsigned int m_number;
+  double m_balance;
+public:
+  Account(int number, double balance=0);
+  void credit(double a);
+  double debit(double a);
+  unsigned int acc();
+  void print(std::ostream &o);
+  ~Account();
+};
+
+class Current_Account: public Account{
+public:
+  Current_Account(int number, double balance=0);
+  double debit(double a);
+  ~Current_Account();
+};
+
+class Saving_Account: public Account{
+protected:
+  double m_interest_rate;
+public:
+  Saving_Account(int number, double balance=0);
+  void add_intersts();
+  ~Saving_Account();
+};
+
+class Blocked_Account: Saving_Account{
+public:
+  Blocked_Account(int number, double balance=0);
+  double debit(double a);
+  ~Blocked_Account();
+};
+
+class Unblocked_Account: Saving_Account{
+public:
+  Unblocked_Account(int number, double balance=0);
+  double debit(double a);
+  ~Unblocked_Account();
+};
+
+#endif
